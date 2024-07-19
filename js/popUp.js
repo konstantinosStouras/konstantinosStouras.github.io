a587c89a3526a468c62b234c6ac027e6d99e92b1
Konstantinos Stouras <konstantinos.stouras@insead.edu>
2024-07-19 20:53:15 +0300

    sdf

---
+++ js/popUp.js
@@ -0,0 +1,8 @@
+function popUpBox(divid){
+	if(document.getElementById(divid).style.display == 'none'){
+		document.getElementById(divid).style.display = 'block';
+		}
+		else{
+		document.getElementById(divid).style.display = 'none';
+		}
+}
--- js/toggleDiv.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-function toggleDiv(divid){
-if(document.getElementById(divid).style.display == 'none'){
-document.getElementById(divid).style.display = 'block';
-}else{
-document.getElementById(divid).style.display = 'none';
-}
-}
-</script>
